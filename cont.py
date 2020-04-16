da915449f943827bb60a5d7a67c3e590afa38b3e
yuktakhatri <54777825+yuktakhatri@users.noreply.github.com>
2020-04-16 22:36:45 +0530

    Create cont.py

---
+++ cont.py
@@ -0,0 +1,12 @@
+import os
+
+for num in range(1,101):
+	if num<=25:
+		os.system("sudo docker run -itd --restart unless-stopped --name adhocnw{} alpine".format(num))
+	elif num<=50:
+		os.system("sudo docker run -itd --restart unless-stopped --name adhocnw{} fedora".format(num))
+	elif num<=75:
+		os.system("sudo docker run -itd --restart unless-stopped --name adhocnw{} centos".format(num))
+	elif num<=100:
+		os.system("sudo docker run -itd --restart unless-stopped --name adhocnw{} java".format(num))
+os.system("sudo docker ps")
